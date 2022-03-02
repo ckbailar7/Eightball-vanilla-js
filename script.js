efc220998c030b0c084909986e78f67448f644bd
ckbailar7 <tkdinst.429897@gmail.com>
2022-03-02 10:02:22 -0500

    Create script.js
    updated comment

---
+++ script.js
@@ -0,0 +1,61 @@
+const userName = 'Ashley';
+
+
+/*Ternanry expression */
+userName ? console.log(`Hello ${userName}!`) : console.log('Hello!')
+
+let userQuestion = 'Will I be rich?';
+
+console.log("The user asked:" + userQuestion);
+
+let randomNumber = Math.floor(Math.random() * 8);
+
+const eightBall = '';
+
+if (randomNumber === 0) {
+  console.log('It is certain')
+} else if (randomNumber === 1) {
+  console.log('It is decidedly so')
+} else if (randomNumber === 2) {
+  console.log('Reply hazy try again')
+} else if (randomNumber === 3) {
+  console.log('Cannot predict now')
+} else if (randomNumber === 4) {
+  console.log('Do not count on it')
+} else if (randomNumber === 5) {
+  console.log('My sources say no')
+} else if (randomNumber === 6) {
+  console.log('Outlook not so good')
+} else {
+  console.log('Signs point to yes')
+}
+
+
+/* Switch statement*/
+/* 
+switch (randomNumber) {
+  case 0:
+  console.log('It is certain')
+  break;
+  case 1:
+  console.log('Reply hazy try again')
+  break;
+  case 2:
+  console.log('Cannot predict now')
+  break;
+  case 3:
+  console.log('Do not count on it')
+  break;
+  case 4:
+  console.log('My sources say no')
+  break;
+  case 5: 
+  console.log('Outlook not so good')
+  break;
+  default:
+  console.log('Signs point to yes!')
+}*/
+
+
+
+
